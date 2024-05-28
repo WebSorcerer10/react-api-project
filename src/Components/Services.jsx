188539b6f59aa37d10d892646453019d071a0139
Shivang Yadav <shivangyadav39@gmail.com>
2024-05-28 16:04:29 +0530

    component life cycle method useeffect() on state change

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,25 +1,58 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "../utils/Axios";
 
 const Services = () => {
+  const [first, setFirst] = useState("this is normal data");
+  const [second, setSecond] = useState("this is very large data");
 
-  const [first,setFirst] = useState("this is normal data");
-  const [second,setSecond] = useState("this is very large data");
-  
+  //these are getting products
+  const getusers = () => {
+    const api = "/users";
 
-    useEffect(() => {
-        console.log("service component is created")
+    //.then we are makiing a response
+    axios
+      .get(api)
+      .then((users) => {
+        console.log(users);
+        
+      })
+      .catch((error) => console.error(error));
+  };
+  useEffect(() => {
+    getusers();
+    console.log("service component is created");
 
-        return () => {
-            console.log("service component is deleted")
-        }
-    });
+    return () => {
+      console.log("service component is deleted");
+    };
+  },[second]);
+  //this square bracket does not allow component rerendering
+  //this [] controls rerendering or updation
+
+  //if we write any state variable inside them[]
+  //then the whole component will be updated on changing
+  //that particular state
+
+    
 
   return (
     <div>
-    <h1> {first} </h1>
-    <button>change normal data</button>
+      <h1> {first} </h1>
+      <button
+        onClick={() => setFirst("Normal data has been changed")}
+        className="rounded bg-red-200 p-2 mb-10"
+      >
+        change normal data
+      </button>
+      <h1> {second} </h1>
+      <button
+        onClick={() => setSecond("Large data has been changed")}
+        className="rounded bg-blue-200 p-2 mb-10"
+      >
+        change large data
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
--- src/Components/Show.jsx
+++ src/Components/Show.jsx
@@ -1,40 +1,47 @@
-import axios from 'axios';
-import React, { useState } from 'react'
+import axios from "../utils/Axios";
+import React, { useState, useEffect } from "react";
 
 const Show = () => {
-    
-    const[products,setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  //these are getting products
+  const getproducts = () => {
+    const api = "/products";
+
+    //.then we are makiing a response
+    axios
+      .get(api)
+      .then((products) => {
+        console.log(products);
+        setProducts(products.data);
+      })
+      .catch((error) => console.error(error));
+  };
 
 
-    //these are getting products
-    const getproducts = () => {
-      const api = "https://fakestoreapi.com/products";
-  
-      //.then we are makiing a response
-      axios
-        .get(api)
-        .then((products) => {
-          console.log(products);
-          setProducts(products.data)
-        })
-        .catch((error) => console.error(error));
-    };
+  useEffect(() => {
+    getproducts();
+  }, []);
+
+
   return (
     <>
       <button onClick={getproducts} className="rounded px-5 py-2 bg-green-300">
         Call product apis
       </button>
-    <ul>
-        {products.length>0 ? products.map( p => 
-        <li key={p.id} className="rounded mb-2  p-5 bg-red-100">
-          {p.title}
-        </li>
-        ):<h1>Loading...</h1>}
-    </ul>        
-    
+      <ul className="pt-5">
+        {products.length > 0 ? (
+          products.map((p) => (
+            <li key={p.id} className="rounded mb-2  p-5 bg-red-100">
+              {p.title}
+            </li>
+          ))
+        ) : (
+          <h1>Loading...</h1>
+        )}
+      </ul>
     </>
-    
-  )
-}
+  );
+};
 
-export default Show
+export default Show;
